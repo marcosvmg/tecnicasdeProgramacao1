dd343a847e7d190aadd92ac8693f75752e83cf25
marcos <marcos@marcos-estudos>
2025-03-23 21:55:29 -0300

    .

---
+++ atividadePratica3Abstracao/src/atividadePratica3Abstracao/AbstracaoGrupos.java
@@ -0,0 +1,118 @@
+package atividadePratica3Abstracao;
+
+public class AbstracaoGrupos {
+
+	public static void main(String[] args) {
+		// Grupo 1
+		class Computador { private String marca; private String processador; private int memoriaRAM; }
+		class Telescopio { private double diametroLente; private String tipoMontagem; }
+		class Girafa { private double altura; private String padraoPelagem; }
+		class Gravura { private String tecnica; private String artista; }
+		class TaxaJuros { private double valor; private String tipo; }
+		class Desenho { private String estilo; private String autor; }
+		class Trem { private int numeroVagoes; private String tipoLocomotiva; }
+		class Tigre { private String especie; private double peso; }
+		class Roteador { private String modelo; private int velocidade; }
+		class Smartwatch { private String marca; private boolean temGPS; }
+		class Professor { private String nome; private String disciplina; }
+		class Recessao { private int anoInicio; private String causa; }
+		class Asteroide { private double diametro; private String composicao; }
+		class Genetica { private String areaEstudo; private String aplicacao; }
+		class Pintura { private String estilo; private String artista; }
+		class Celula { private String tipo; private String funcao; }
+		class Osmose { private String tipo; private String aplicacao; }
+		class IdadeMedia { private int anoInicio; private int anoFim; }
+		class ArteDigital { private String software; private String artista; }
+		class Cinema { private String genero; private int duracaoMinutos; }
+
+		// Grupo 2
+		class Smartphone { private String marca; private String sistemaOperacional; }
+		class ViaLactea { private double diametroAnosLuz; private String tipoGalaxia; }
+		class CameraDigital { private int resolucaoMP; private String tipoLente; }
+		class Elefante { private String especie; private double peso; }
+		class Carro { private String marca; private String modelo; }
+		class Motocicleta { private String marca; private int cilindradas; }
+		class ImperioRomano { private int anoFundacao; private int anoQueda; }
+		class Aviao { private String fabricante; private int capacidadePassageiros; }
+		class Fotossintese { private String tipoPlanta; private String pigmento; }
+		class Danca { private String estilo; private String origem; }
+		class IdadeBronze { private int anoInicio; private int anoFim; }
+		class PiramidesEgito { private String localizacao; private int alturaMetros; }
+		class ChefCozinha { private String nome; private String especialidade; }
+		class Galaxia { private String nome; private double distanciaTerraAnosLuz; }
+		class ConservacaoEnergia { private String principio; private String aplicacao; }
+		class Escultura { private String material; private String artista; }
+		class Monitor { private int polegadas; private String resolucao; }
+		class Leao { private String especie; private double peso; }
+		class Moeda { private String pais; private double valor; }
+		class Pintor { private String nome; private String estilo; }
+
+		// Grupo 3
+		class Renascimento { private int anoInicio; private int anoFim; }
+		class Coelho { private String especie; private double peso; }
+		class Nebulosa { private String tipo; private double distanciaTerraAnosLuz; }
+		class Gravidade { private double constante; private String unidade; }
+		class Evolucao { private String teoria; private String cientista; }
+		class Helicoptero { private String fabricante; private int capacidadePassageiros; }
+		class Aguia { private String especie; private double envergadura; }
+		class Inflacao { private double taxaAnual; private String causa; }
+		class Jornalista { private String nome; private String veiculo; }
+		class Fotografia { private String tecnica; private String fotografo; }
+		class Tablet { private String marca; private int polegadas; }
+		class ReflexoCondicionado { private String tipo; private String cientista; }
+		class MercadoAcoes { private String bolsa; private double indice; }
+		class Engenheiro { private String nome; private String especialidade; }
+		class Barco { private String tipo; private int capacidadePassageiros; }
+		class Ator { private String nome; private String filmeFamoso; }
+		class RevolucaoFrancesa { private int anoInicio; private int anoFim; }
+		class Biologo { private String nome; private String areaEstudo; }
+		class Bicicleta { private String marca; private int marchas; }
+		class Cao { private String raca; private double peso; }
+
+		// Grupo 4
+		class BolsaValores { private String nome; private double indice; }
+		class Trator { private String marca; private int potenciaHP; }
+		class Gato { private String raca; private double peso; }
+		class ReacaoQuimica { private String tipo; private String produtos; }
+		class Medico { private String nome; private String especialidade; }
+		class EraViking { private int anoInicio; private int anoFim; }
+		class Importacao { private String produto; private double valor; }
+		class Teclado { private String tipo; private int numeroTeclas; }
+		class Caminhao { private String marca; private int capacidadeCarga; }
+		class Golfinho { private String especie; private double peso; }
+		class OrcamentoPublico { private double valor; private String anoFiscal; }
+		class InstalacaoArtistica { private String artista; private String local; }
+		class PrimeiraGuerraMundial { private int anoInicio; private int anoFim; }
+		class Atom { private String simbolo; private int numeroAtomico; }
+		class PIB { private double valor; private String pais; }
+		class DescobrimentoAmerica { private int ano; private String navegador; }
+		class SateliteNatural { private String nome; private String planetaOrbita; }
+		class Mouse { private String marca; private boolean semFio; }
+		class RevolucaoIndustrial { private int anoInicio; private int anoFim; }
+		class Advogado { private String nome; private String areaAtuacao; }
+
+		// Grupo 5
+		class Impressora { private String marca; private String tecnologia; }
+		class BuracoNegro { private double massa; private double raio; }
+		class Planeta { private String nome; private double distanciaSol; }
+		class Serpente { private String especie; private double comprimento; }
+		class Ceramica { private String tipo; private String origem; }
+		class Cometa { private String nome; private double periodoOrbital; }
+		class Estrela { private String nome; private double magnitude; }
+		class Onibus { private String marca; private int capacidadePassageiros; }
+		class Exportacao { private String produto; private double valor; }
+		class Dentista { private String nome; private String especialidade; }
+		class Jaqueta { private String material; private String cor; }
+		class Botas { private String material; private String tipo; }
+		class Chapeu { private String estilo; private String material; }
+		class Gravata { private String cor; private String padrao; }
+		class Vestido { private String estilo; private String cor; }
+		class Saia { private String comprimento; private String material; }
+		class TenisVestuario { private String marca; private String cor; }
+		class Calca { private String tipo; private String material; }
+		class Camisa { private String estilo; private String cor; }
+		class Sueter { private String material; private String cor; }
+
+	}
+
+}
